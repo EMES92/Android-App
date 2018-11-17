f2784593547eb12dc63359ecc042b8165ec16e0b
EMES92 <valepk-92@hotmail.it>
2018-11-17 11:13:09 +0100

    inseriti tutti i comandi attualmente sviluppati

--- app/src/main/java/com/example/valerio/myapplication/MainActivity.java
+++ app/src/main/java/com/example/valerio/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.valerio.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -9,12 +8,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
-
-import org.telegram.passport.PassportScope;
-import org.telegram.passport.PassportScopeElementOne;
-import org.telegram.passport.PassportScopeElementOneOfSeveral;
-import org.telegram.passport.TelegramLoginButton;
-import org.telegram.passport.TelegramPassport;
+import android.support.v7.app.AppCompatActivity;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -29,10 +23,15 @@ import com.loopj.android.http.*;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
-    private TelegramLoginButton loginButton;
     Button button1;
     Button button2;
-    @Override
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    //@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -42,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         button1 = (Button) findViewById(R.id.btn1);
         button2 = (Button) findViewById(R.id.btn2);
+        button3 = (Button) findViewById(R.id.btn3);
+        button4 = (Button) findViewById(R.id.btn4);
+        button5 = (Button) findViewById(R.id.btn5);
+        button6 = (Button) findViewById(R.id.btn6);
+        button7 = (Button) findViewById(R.id.btn7);
+        button8 = (Button) findViewById(R.id.btn8);
 
         button1.setOnClickListener(new OnClickListener() {
 
@@ -72,6 +77,168 @@ public class MainActivity extends AppCompatActivity {
         });
 
         button2.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "meteo";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button3.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "servo";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button4.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "temperatura";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button5.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "noise";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button6.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "light";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button7.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                String apiToken = "603981379:AAFux3TNvauoPfIN0a1BWAnQ60IRcWg3Oos"; //'"my_bot_api_token";EmesAndroid Bot
+                String chatId = "-1001476670474";//"@my_channel_name";  // id private = -1001476670474 del canale domestico
+                String text = "sisma";
+                urlString = String.format(urlString, apiToken, chatId, text);
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams params = new RequestParams();
+                params.put("key", "value");
+                params.put("more", "data");
+                client.get(urlString, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
+            }
+
+        });
+        button8.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
